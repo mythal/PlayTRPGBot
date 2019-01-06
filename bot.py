6564effae407841470dd6737d9cdb2605bacae03
Koppa <t@ioover.net>
2019-01-06 13:06:00 +0800

    settings for deploy

--- bot.py
+++ bot.py
@@ -415,7 +415,12 @@ def main():
     dp.add_error_handler(error)
 
     # Start the Bot
-    updater.start_polling()
+    if 'WEBHOOK_URL' in os.environ:
+        updater.start_webhook(listen='0.0.0.0', port=9990, url_path=TOKEN)
+        url = os.path.join(os.environ['WEBHOOK_URL'], TOKEN)
+        updater.bot.set_webhook(url=url)
+    else:
+        updater.start_polling()
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
