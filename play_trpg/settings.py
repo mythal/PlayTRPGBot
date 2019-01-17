8a6474146fd58e01f47e8383dffdc7fa6fa47dab
Koppa <t@ioover.net>
2019-01-17 16:20:16 +0800

    fix redis host

--- play_trpg/settings.py
+++ play_trpg/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
+        "LOCATION": "redis://redis:6379/1",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
             "PICKLE_VERSION": -1,  # Use the latest protocol version
