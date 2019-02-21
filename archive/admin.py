e7ced4bd43522c2960fed5e4dccbf1e896978c80
Koppa <t@ioover.net>
2019-02-21 11:37:22 +0800

    log filter by chat

--- archive/admin.py
+++ archive/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
+
 from .models import Log, Chat
 
 
 @admin.register(Log)
 class LogAdmin(admin.ModelAdmin):
     date_hierarchy = 'created'
+    list_filter = ('chat',)
     list_display = ('chat', 'character_name', 'content', 'created')
     list_display_links = ('content',)
     search_fields = ('chat__title', 'content')
