4001409efa805add667d2c5e55b16dbf05e1235c
Quan Brew <t@ioover.net>
2019-05-31 03:38:49 +0800

    improve schema

--- archive/schema.py
+++ archive/schema.py
@@ -20,7 +20,13 @@ class Chat(DjangoObjectType):
     class Meta:
         model = models.Chat
         exclude_fields = ('password', 'log_set')
-    logs = graphene.List(Log, password=graphene.String(), start_date=graphene.DateTime(), end_date=graphene.DateTime())
+    logs = graphene.List(
+        Log,
+        password=graphene.String(),
+        start_date=graphene.DateTime(),
+        end_date=graphene.DateTime(),
+        count=graphene.Int(),
+    )
     has_password = NonNull(graphene.Boolean)
     counter = NonNull(graphene.Int)
 
@@ -29,13 +35,18 @@ class Chat(DjangoObjectType):
         return bool(chat.password)
 
     @staticmethod
-    def resolve_logs(chat: models.Chat, info, password='', start_time=None, end_time=None):
-        start_time = start_time or datetime.datetime.now()
-        end_time = end_time or datetime.datetime.min
+    def resolve_logs(chat: models.Chat, info, password='', start_time=None, end_time=None, count=256):
         if not chat.password or (chat.password and chat.validate(password)):
-            return chat.all_log().filter(created__gte=end_time, created__lte=start_time).all()
+            query_set = chat.all_log()
+            if start_time:
+                query_set = query_set.filter(created__lte=start_time)
+            if end_time:
+                query_set = query_set.filter(created__gte=end_time)
+            if count:
+                query_set = query_set[:count]
+            return query_set
         else:
-            return []
+            return None
 
     @staticmethod
     def resolve_counter(chat: models.Chat, info):
@@ -44,10 +55,15 @@ class Chat(DjangoObjectType):
 
 class Query(graphene.ObjectType):
     chats = graphene.List(Chat)
+    chat = graphene.Field(Chat, id=graphene.Int())
 
     @staticmethod
     def resolve_chats(root, info):
         return models.Chat.objects.filter(parent=None).all()
+
+    @staticmethod
+    def resolve_chat(root, info, id):
+        return models.Chat.objects.get(id=id)
 
 
 schema = graphene.Schema(query=Query)
