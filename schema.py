aa9c4398a513a342d02ef001671b613bf959662c
Quan Brew <t@ioover.net>
2019-08-08 17:20:33 +0800

    add more GraphQL API

--- schema.py
+++ schema.py
@@ -1,101 +1,159 @@
-from typing import Optional
+# Graphene Documents https://graphene-python.org/
+from typing import Optional, List
+import datetime
 
 import graphene
 from graphene import NonNull
 from graphene_django import DjangoObjectType
 from django.core.cache import cache
+from django.http import HttpRequest
 
-from archive import models
+from archive import models as archive
+from game import models as game
+from user import models as user
 
-Kind = graphene.Enum.from_enum(models.LogKind)
+Kind = graphene.Enum.from_enum(archive.LogKind)
+
+
+def get_telegram_profile(request: HttpRequest) -> Optional[user.TelegramProfile]:
+    if not request.user.is_authenticated:
+        return None
+    return getattr(request.user, 'telegram', None)
 
 
 class Tag(DjangoObjectType):
     class Meta:
-        model = models.Tag
+        model = archive.Tag
 
 
 class Log(DjangoObjectType):
     class Meta:
-        model = models.Log
+        model = archive.Log
         exclude_fields = ('user_id', 'log_set', 'deleted')
 
     kind = Kind()
     entities = graphene.String(required=True)
 
 
-class LogList(graphene.ObjectType):
-    records = graphene.List(NonNull(Log), required=True)
-    total_page = graphene.Int(required=True)
-    tag_name = graphene.String()
-    modified = graphene.DateTime()
+class Player(DjangoObjectType):
+    class Meta:
+        model = game.Player
+
+
+class Variable(DjangoObjectType):
+    class Meta:
+        model = game.Variable
+
+
+class VariableMutation(graphene.Mutation):
+    class Arguments:
+        telegram_chat_id = graphene.ID(required=True)
+        variable_id = graphene.ID(name='id')
+        name = graphene.String()
+        value = graphene.String()
+        group = graphene.String()
+
+    variable = graphene.Field(Variable)
+    error = graphene.String()
+
+    def mutate(self, info, telegram_chat_id, variable_id=None, name='', value='', group=''):
+        my_profile = get_telegram_profile(info.context)
+        if not my_profile:
+            return VariableMutation(variable=None, error='You not logged in.')
+        my_player = game.Player.objects.get(chat_id=telegram_chat_id, user_id=my_profile.user_id)
+        if variable_id:
+            variable = game.Variable.objects.get(pk=variable_id)
+            if variable.player_id != my_player.id and not my_player.is_gm:
+                return VariableMutation(variable=None, error='No permission.')
+            if name:
+                variable.name = name
+            if value:
+                variable.value = value
+            if group:
+                variable.group = group
+            variable.save()
+        else:
+            if not name:
+                return VariableMutation(variable=None, error='Create variable needs name.')
+            variable = game.Variable(player=my_player, name=name, value=value, group=group)
+            variable.save()
+        return VariableMutation(variable=variable)
 
 
 class Chat(DjangoObjectType):
-    page_limit = 256
-
     class Meta:
-        model = models.Chat
+        model = archive.Chat
         exclude_fields = ('password', 'parent')
 
-    log_list = graphene.Field(LogList, password=graphene.String(), page=graphene.Int(), tag_id=graphene.String())
+    log_list = graphene.Field(graphene.List(NonNull(Log), required=True), password=graphene.String())
     is_require_password = graphene.Boolean(required=True)
     counter = graphene.Int(required=True)
     page_counter = graphene.Int(required=True)
+    players = graphene.Field(graphene.List(NonNull(Player), required=True))
 
     @staticmethod
-    def resolve_is_require_password(chat: models.Chat, info):
+    def resolve_players(chat: archive.Chat, info):
+        return game.Player.objects.filter(chat_id=chat.chat_id).all()
+
+    @staticmethod
+    def resolve_is_require_password(chat: archive.Chat, info):
         return bool(chat.password)
 
     @staticmethod
-    def resolve_page_counter(chat: models.Chat, info):
-        return chat.query_log().count() % Chat.page_limit
+    def resolve_log_list(chat: archive.Chat, info, password=''):
+        log_list_cache_time_key = 'api:chat:{}:log:cache_time'.format(chat.id)
+        log_list_key = 'api:chat:{}:log'.format(chat.id)
+        log_list_cache_time: datetime.datetime = cache.get(log_list_cache_time_key, datetime.datetime.min)
+        log_list: Optional[List[archive.Log]] = cache.get(log_list_key)
 
-    @staticmethod
-    def resolve_log_list(chat: models.Chat, info, password='', tag_id=None, page=1):
-        limit = Chat.page_limit
-        offset = (page - 1) * limit
-        tag_name = None
-        if page < 1 or (chat.password and not chat.validate(password)):
+        if not chat.validate(password):
             return None
 
-        cached_log_list_key = 'graphql:chat:{}:log:tag:{}:page:{}'.format(chat.id, tag_id or '_', page)
-        cached_log_list: Optional[LogList] = cache.get(cached_log_list_key)
-        if cached_log_list:
-            if chat.modified == cached_log_list.modified:
-                return cached_log_list
+        if log_list and log_list_cache_time > chat.modified:
+            return log_list
+        log_list = list(chat.query_log().all())
 
-        query_set = chat.query_log()
-        if tag_id:
-            tag: Optional[models.Tag] = models.Tag.objects.filter(id=tag_id, chat=chat).first()
-            if tag:
-                query_set = tag.log_set.all()
-        log_counter = query_set.count()
-        log_list = LogList(
-            tag_name=tag_name,
-            records=query_set[offset:offset+limit].all(),
-            total_page=log_counter // Chat.page_limit + 1,
-            modified=chat.modified,
-        )
-        cache.set(cached_log_list_key, log_list, 1 * 60 * 60)
+        cache.set(log_list_key, log_list, 5 * 24 * 60 * 60)
+        cache.set(log_list_cache_time_key, datetime.datetime.now())
         return log_list
 
     @staticmethod
-    def resolve_counter(chat: models.Chat, info):
+    def resolve_counter(chat: archive.Chat, info):
         return chat.log_count()
+
+
+class TelegramProfile(DjangoObjectType):
+    player_set = graphene.Field(graphene.List(Player, required=True))
+
+    @staticmethod
+    def resolve_player_set(profile: user.TelegramProfile, info):
+        return game.Player.objects.filter(user_id=profile.telegram_id).all()
+
+    class Meta:
+        model = user.TelegramProfile
 
 
 class Query(graphene.ObjectType):
     chats = graphene.List(NonNull(Chat), required=True)
-    chat = graphene.Field(Chat, id=graphene.Int(required=True))
+    chat = graphene.Field(Chat, chat_id=graphene.ID(name='id', required=True))
+    player = graphene.Field(Player, player_id=graphene.ID(name='id', required=True))
+    my_profile = graphene.Field(TelegramProfile)
+
+    @staticmethod
+    def resolve_my_profile(root, info):
+        return get_telegram_profile(info.context)
 
     @staticmethod
     def resolve_chats(root, info):
-        return models.Chat.objects.filter(parent=None).all()
+        return archive.Chat.objects.filter(parent=None).all()
 
     @staticmethod
-    def resolve_chat(root, info, id):
-        return models.Chat.objects.get(id=id)
+    def resolve_chat(root, info, chat_id: str):
+        return archive.Chat.objects.filter(id=chat_id).first()
+
+    @staticmethod
+    def resolve_player(root, info, player_id: str):
+        return game.Player.objects.filter(id=player_id).first()
 
 
 schema = graphene.Schema(query=Query)
